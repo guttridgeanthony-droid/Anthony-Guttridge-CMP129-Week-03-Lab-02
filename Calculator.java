1b59f26e923f9a40e87cf90372b482ff5b3959e4
guttridgeanthony-droid <guttridge.anthony@student.ccm.edu>
2026-09-25 13:48:36 -0400

    Complete Week 3 Lab 2

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,32 @@
+/*
+ * Student Name: Anthony Guttridge
+ * Course: CMP 129
+ * Week: 3
+ * Lab: 2
+ * Assignment: Calculator Method Overloading
+ * Date: 9/25/2026
+ */
+
+
 public class Calculator {
-    
+
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+
+    public String add(String a, String b) {
+        return a + b;
+    }
 }
--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,3 +1,24 @@
-public class CalculatorTest {
-    
+/*
+ * Student Name:
+ * Course: CMP 129
+ * Week: 3
+ * Lab: 2
+ * Assignment: Calculator Method Overloading
+ * Date:
+ */
+
+ class CalculatorTest {
+
+    public static void main(String[] args) {
+        // Instantiate the Calculator object
+        Calculator calc = new Calculator();
+        int sumTwoInts = calc.add(10, 5);
+        System.out.println("Sum of two integers: " + sumTwoInts);
+        double sumTwoDoubles = calc.add(6.5, 6.75);
+        System.out.println("Sum of two doubles: " + sumTwoDoubles);
+        int sumThreeInts = calc.add(10, 10, 10);
+        System.out.println("Sum of three integers: " + sumThreeInts);
+        String concatenatedString = calc.add("Hello ", "World");
+        System.out.println("Concatenated strings: " + concatenatedString);
+    }
 }
